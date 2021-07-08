ceb57493a742de45f5855c18aeedb912812d3728
Francois Ortelli <francois.ortelli2@gmail.com>
2021-07-08 14:24:26 +0200

    fin du jour 4

--- script.js
+++ script.js
@@ -1,14 +1,67 @@
-document.getElementsByClassName('counter')[0].textContent = document.getElementsByClassName('rowMessage').length
 
-for(var i=0; i<document.getElementsByClassName('poubelle').length; i++) {
 
-    document.getElementsByClassName('poubelle')[i].addEventListener("click",
+// fonction qui s'occupe de mettre les diiferent élément JS de la page
+var refresh = () => { 
+
+    // compte le nombre de message dans la liste
+    document.getElementsByClassName('counter')[0].textContent = document.getElementsByClassName('rowMessage').length
+
+    for(var i=0; i<document.getElementsByClassName('poubelle').length; i++) {
+    // Supprime le message et met a jour le compteur.
+      document.getElementsByClassName('poubelle')[i].addEventListener("click",
+        function(){
+          this.parentNode.remove()
+          document.getElementsByClassName('counter')[0].textContent = document.getElementsByClassName('rowMessage').length
+        }   
+     );
+   }
+}
+
+
+// initialise la page
+refresh();
+
+
+// creation de nouveau message au click sur Add
+document.getElementById('addmessage').addEventListener("click",
       function(){
-        this.parentNode.remove()
-        document.getElementsByClassName('counter')[0].textContent = document.getElementsByClassName('rowMessage').length
+        // test sur la console
+        console.log('message reçu captain !!!')
 
+      // création des nouveaux éléments (div,p,h4..)
+        var newDiv = document.createElement("div");
+        var avat = document.createElement("img");
+        var newDiv1 = document.createElement("div");
+        var nomAuthor = document.createElement("h6");
+        var newP = document.createElement("p");
+        var bin = document.createElement("img");
 
-    }
-        
-    );
-  }
+      //récupération du contenue du message et nom de l'auteur
+        var saisie = document.getElementById('inputtext').value;
+        var idAuthor = 'Ortelli Francois';
+
+      // set attribut aux elements
+        newDiv.setAttribute("class", "row rowMessage");
+        avat.setAttribute("class", "avatar");
+        avat.setAttribute("src", "/ressource/avatar-4.jpg");
+        newDiv1.setAttribute("class", "textinfo");
+        nomAuthor.textContent= idAuthor;
+        newP.textContent= saisie;
+        bin.setAttribute("class", "poubelle");
+        bin.setAttribute("src", "/ressource/trash.png");
+
+      // ajoute les element crées aux parents 
+        document.body.appendChild(newDiv);
+        newDiv.appendChild(avat)
+        newDiv.appendChild(newDiv1)
+        newDiv.appendChild(bin)
+        newDiv1.appendChild(nomAuthor)
+        newDiv1.appendChild(newP)
+
+      //je recompte les messages et set les poubelles
+        refresh();
+
+      // clear le champs de saisie
+      document.getElementById('inputtext').value = "";
+    } 
+    )
